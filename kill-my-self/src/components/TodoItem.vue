5597a3148510db2afd1c88f6fc8deb812b13a319
who-is-Nils <e4sy3456@gmail.com>
2025-11-03 16:54:32 +0100

    anpassung text farbe

--- kill-my-self/src/components/TodoItem.vue
+++ kill-my-self/src/components/TodoItem.vue
@@ -5,7 +5,7 @@
     <p class="text-sm text-blue-600 font-medium">Priorität: {{ prio }}</p>
     <button
         @click="$emit('delete')"
-        class="mt-2 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+        class="mt-2 bg-red-500 text-black px-3 py-1 rounded hover:bg-red-600"
     >
       Löschen
     </button>
