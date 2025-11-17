05d48be8bf304e63a694d10b61d28ac91113b3e9
who-is-Nils <e4sy3456@gmail.com>
2025-11-17 17:08:02 +0100

    typescript linter

--- kill-my-self/eslint.config.mjs
+++ kill-my-self/eslint.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
-export default defineConfig(
+export default defineConfig([
     eslint.configs.recommended,
-    tseslint.configs.recommended,
-);
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    {
+        rules: {
+            "@typescript-eslint/consistent-type-definitions": "off",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-dynamic-delete": "off",
+        },
+    },
+]);
